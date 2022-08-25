a603363b0fa2e602d1959cc2e386ed202170dc5d
Johnson Iyida <johnson.iyida@venturegardengroup.com>
2022-08-25 03:42:19 +0100

    White test for Greet component

---
+++ src/components/greet/Greet.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type GreetProps = {
+  name?: string
+}
+
+export default function Greet(props: GreetProps) {
+  return (
+    <div>Hello {props.name}</div>
+  )
+}
---
+++ src/components/greet/greet.test.tsx
@@ -0,0 +1,14 @@
+import {render, screen} from '@testing-library/react';
+import Great from './Greet'
+
+test('Should render correctly', () => { 
+    render(<Great />);
+    const textElement = screen.getByText(/hello/i);
+    expect(textElement).toBeInTheDocument();
+ })
+
+ test('Should render correctly', () => { 
+    render(<Great name="dili" />);
+    const textElement = screen.getByText(/hello dili/i);
+    expect(textElement).toBeInTheDocument();
+ })
